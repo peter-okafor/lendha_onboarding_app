da51b84292d965b5dba3af8aea9c1fd17066fdb8
Neezah <Neezah_snow@yahoo.com>
2023-02-09 15:42:10 +0100

    feat: logo-spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { Spinner } from '@chakra-ui/react';
 import { Suspense } from 'react';
 import LendhaRouter from './routes';
 import RemoveTrailingSlash from './utils/components/RemoveTrailingSlash';
+import logo from '@/assets/svg/logo/logo-blue-mobile.svg';
 
 const App = () => {
   return (
-    <Suspense fallback={<p> Loading...</p>}>
-      <RemoveTrailingSlash />
-      <LendhaRouter />
+    <Suspense
+      fallback={
+        <p
+          style={{
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            transform: 'translate(-50%, -50%)'
+          }}
+        >
+          <Spinner size='xl'>
+            <img src={logo} alt='logo' width='150px' height='150px' />
+          </Spinner>
+        </p>
+      }
+    >
+      <>
+        <RemoveTrailingSlash />
+        <LendhaRouter />
+      </>
     </Suspense>
   );
 };
