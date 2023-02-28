b25b0012db68b0b8c284ecddc2152ca9be0508cc
JoshX <joshx.dev@gmail.com>
2023-02-28 12:11:59 +0100

    feat(review): remove guarantor form on take loan page

--- src/features/onfield-credit-officer/features/loans/LoanApply.tsx
+++ src/features/onfield-credit-officer/features/loans/LoanApply.tsx
@@ -3,7 +3,7 @@ import { Alert, Card } from '@/components/common';
 import { LoanCalculator } from '@/components/loans';
 import {
   TakeLoanDetails,
-  TakeLoanGuarantorForm,
+  // TakeLoanGuarantorForm,
   TakeLoanLinkBankStatement
 } from '@/components/loans';
 import { path } from '@/routes/path';
@@ -128,13 +128,13 @@ const LoanApply = () => {
             {step === 'bank' && (
               <TakeLoanLinkBankStatement
                 onBack={() => setStep('form')}
-                onSubmit={() => setStep('guarantor')}
+                onSubmit={() => setStep('confirm')}
                 showCancelBtn={!isLargerThan810 ? true : false}
                 showSocialHandles={false}
               />
             )}
 
-            {step === 'guarantor' && (
+            {/* {step === 'guarantor' && (
               <TakeLoanGuarantorForm
                 onBack={() => setStep('bank')}
                 onSubmit={() => setStep('confirm')}
@@ -147,7 +147,7 @@ const LoanApply = () => {
                 }}
                 showCancelBtn={!isLargerThan810 ? true : false}
               />
-            )}
+            )} */}
 
             {step === 'confirm' && (
               <TakeLoanDetails
